617a87e91621c75559b21d117c7b9a59e667f6b1
meng.xie <meng.xie@tongdun.cn>
2019-09-29 17:58:05 +0800

    dd

---
+++ 线上问题排查/src/main/java/org/muzhu/StackProblem.java
@@ -0,0 +1,18 @@
+package org.muzhu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @auther meng.xie
+ * 2019/9/24.
+ */
+public class StackProblem {
+    private static final Logger LOG = LoggerFactory.getLogger(StackProblem.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ) {
+
+        }
+    }
+}
